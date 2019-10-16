c5e1f2d239909f3ae8f2c4d123b302d2e256c3e2
John Parnell <john.parnell@ecclesiastical.com>
2019-10-16 10:43:58 +0100

    Initial Commit

---
+++ src/main/java/com/pluralsight/functionalexamples/FunctionalExamples.java
@@ -0,0 +1,21 @@
+package com.pluralsight.functionalexamples;
+
+
+public class FunctionalExamples {
+
+    public static void FunctionalExample1() {
+
+        System.out.println("testing 1");
+
+        System.out.println();
+    }
+
+    public static void FunctionalExample2() {
+
+        System.out.println("testing 2");
+
+        System.out.println();
+    }
+
+
+}
---
+++ src/test/java/com/pluralsight/functionalexamples/FunctionalExample.java
@@ -0,0 +1,25 @@
+package com.pluralsight.functionalexamples;
+
+import static org.junit.Assert.*;
+
+import com.pluralsight.functionalexamples.FunctionalExamples;
+import org.junit.Test;
+
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+
+public class FunctionalExample {
+
+    @Test
+public void FunctionalExample1(){
+        FunctionalExamples.FunctionalExample1();
+
+        FunctionalExamples.FunctionalExample2();
+
+
+    }
+
+
+
+}
